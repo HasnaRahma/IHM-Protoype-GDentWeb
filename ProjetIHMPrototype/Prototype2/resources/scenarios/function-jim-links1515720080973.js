4467534b7984311b18e4fb8f1af49d7a85305f78
BedlaHasnaRahma <eh_bedla@esi.dz>
2019-11-05 14:43:26 +0100

    a

---
+++ ProjetIHMPrototype/Prototype2/resources/scenarios/function-jim-links1515720080973.js
@@ -0,0 +1,422 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "778d2193-93db-4a86-ad0f-592aaf71bc66" : {
+      "Button_2" : [
+        "8ebd9c72-6050-4337-bf89-2978db6b8cfa"
+      ],
+      "Image_1" : [
+        "92ae838d-3135-4b47-a6ee-4ef187ea62ef"
+      ],
+      "Image_2" : [
+        "cd84153f-7f6e-479a-92c2-575c656303f6"
+      ]
+    },
+    "92ae838d-3135-4b47-a6ee-4ef187ea62ef" : {
+      "Button_1" : [
+        "778d2193-93db-4a86-ad0f-592aaf71bc66"
+      ]
+    },
+    "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8" : {
+      "Button_2" : [
+        "5e679026-c831-4fc9-a5dd-9e8c97172b74"
+      ],
+      "Image_1" : [
+        "af92aa72-dca8-41d3-8fd8-7df6685c6618"
+      ],
+      "Image_2" : [
+        "56cc956b-de07-41c2-96f5-64ec7af5467c"
+      ]
+    },
+    "d59a9587-c981-42a2-a93d-cd5e34dcb3a9" : {
+      "Two-line-item_21" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ],
+      "Two-line-item_35" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Menu_item_1" : [
+        "6f3e98f0-6f51-40b0-84eb-8e0a743f0c3b"
+      ],
+      "Image_71" : [
+        "6f3e98f0-6f51-40b0-84eb-8e0a743f0c3b"
+      ],
+      "Input_6" : [
+        "02e8f840-27b7-4797-b1f5-d637b2e90d79"
+      ],
+      "Image_3" : [
+        "cd84153f-7f6e-479a-92c2-575c656303f6"
+      ],
+      "Button_2" : [
+        "8ebd9c72-6050-4337-bf89-2978db6b8cfa"
+      ],
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ]
+    },
+    "d69a4cbf-1844-45c3-9db6-887cad5a0d1b" : {
+      "Button_1" : [
+        "5d024353-5558-47f0-81d7-4ee7aa581184"
+      ],
+      "Button_2" : [
+        "5d024353-5558-47f0-81d7-4ee7aa581184"
+      ]
+    },
+    "c6af2bd8-0711-4b95-89f1-8a9c0036ded7" : {
+      "Button_1" : [
+        "5d024353-5558-47f0-81d7-4ee7aa581184"
+      ]
+    },
+    "8ebd9c72-6050-4337-bf89-2978db6b8cfa" : {
+      "Button_1" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ],
+      "Button_2" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ]
+    },
+    "6f3e98f0-6f51-40b0-84eb-8e0a743f0c3b" : {
+      "Two-line-item_21" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ],
+      "Two-line-item_35" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Image_1" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ],
+      "Image_8" : [
+        "967f078a-a1ed-4ea3-b23b-a865ef80368f"
+      ],
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ]
+    },
+    "4ced58b7-bdbc-43a0-bb6b-443b52e00007" : {
+      "Button_1" : [
+        "0447e155-5a47-452b-95d9-5d9daad2ce6e"
+      ],
+      "Button_2" : [
+        "0447e155-5a47-452b-95d9-5d9daad2ce6e"
+      ]
+    },
+    "3b9b988f-6a4e-40e9-b0fd-28bdbe5193e3" : {
+      "Button_1" : [
+        "0447e155-5a47-452b-95d9-5d9daad2ce6e"
+      ],
+      "Button_2" : [
+        "0447e155-5a47-452b-95d9-5d9daad2ce6e"
+      ]
+    },
+    "1c172a7b-2785-4d33-a1b9-fd5bced26dde" : {
+      "Button_1" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ],
+      "Button_2" : [
+        "778d2193-93db-4a86-ad0f-592aaf71bc66"
+      ]
+    },
+    "1c4f49c6-a53c-4d15-bb26-529d86cf97d5" : {
+      "Two-line-item_21" : [
+        "6f3e98f0-6f51-40b0-84eb-8e0a743f0c3b"
+      ],
+      "Two-line-item_35" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ]
+    },
+    "cd84153f-7f6e-479a-92c2-575c656303f6" : {
+      "Button_1" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ],
+      "Button_2" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ]
+    },
+    "fd20adf0-5afd-4d50-af13-07534c210383" : {
+      "Button_1" : [
+        "5d024353-5558-47f0-81d7-4ee7aa581184"
+      ],
+      "Button_2" : [
+        "5d024353-5558-47f0-81d7-4ee7aa581184"
+      ]
+    },
+    "e4d54d0e-c142-4798-9b6d-5f8e454cd47b" : {
+      "Two-line-item_21" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ],
+      "Two-line-item_35" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Menu_item_7" : [
+        "02e8f840-27b7-4797-b1f5-d637b2e90d79"
+      ],
+      "Menu_item_3" : [
+        "8cd12e76-df4a-4aaa-b195-c0e1ca1794ab"
+      ],
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ]
+    },
+    "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6" : {
+      "Label_7" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_14" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_17" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ],
+      "Button_1" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ]
+    },
+    "b81af61c-0596-4e8a-b6cc-0b9d0265368a" : {
+      "Button_2" : [
+        "4ced58b7-bdbc-43a0-bb6b-443b52e00007"
+      ],
+      "Image_2" : [
+        "3b9b988f-6a4e-40e9-b0fd-28bdbe5193e3"
+      ]
+    },
+    "9d1b7a89-43da-48f3-9a62-02728d968463" : {
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ],
+      "Label_28" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Button_1" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ]
+    },
+    "af92aa72-dca8-41d3-8fd8-7df6685c6618" : {
+      "Button_1" : [
+        "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8"
+      ]
+    },
+    "699d2dd2-f446-420f-98b6-a1763009899b" : {
+      "Button_1" : [
+        "b81af61c-0596-4e8a-b6cc-0b9d0265368a"
+      ],
+      "Button_2" : [
+        "b81af61c-0596-4e8a-b6cc-0b9d0265368a"
+      ]
+    },
+    "02e8f840-27b7-4797-b1f5-d637b2e90d79" : {
+      "Two-line-item_21" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ],
+      "Two-line-item_35" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Menu_item_6" : [
+        "0447e155-5a47-452b-95d9-5d9daad2ce6e"
+      ],
+      "Menu_item_5" : [
+        "e4d54d0e-c142-4798-9b6d-5f8e454cd47b"
+      ],
+      "Menu_item_3" : [
+        "8cd12e76-df4a-4aaa-b195-c0e1ca1794ab"
+      ],
+      "Image_71" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ],
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ]
+    },
+    "0447e155-5a47-452b-95d9-5d9daad2ce6e" : {
+      "Menu_item_7" : [
+        "02e8f840-27b7-4797-b1f5-d637b2e90d79"
+      ],
+      "Menu_item_5" : [
+        "e4d54d0e-c142-4798-9b6d-5f8e454cd47b"
+      ],
+      "Menu_item_3" : [
+        "8cd12e76-df4a-4aaa-b195-c0e1ca1794ab"
+      ],
+      "Image_71" : [
+        "d59a9587-c981-42a2-a93d-cd5e34dcb3a9"
+      ],
+      "Button_2" : [
+        "4ced58b7-bdbc-43a0-bb6b-443b52e00007"
+      ],
+      "Image_10" : [
+        "3b9b988f-6a4e-40e9-b0fd-28bdbe5193e3"
+      ]
+    },
+    "98d9cda0-26f9-4ddd-81be-b6a5c51b0267" : {
+      "Button_1" : [
+        "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8"
+      ],
+      "Button_2" : [
+        "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8"
+      ]
+    },
+    "967f078a-a1ed-4ea3-b23b-a865ef80368f" : {
+      "Two-line-item_21" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ],
+      "Two-line-item_35" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Image_71" : [
+        "6f3e98f0-6f51-40b0-84eb-8e0a743f0c3b"
+      ],
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ]
+    },
+    "8cd12e76-df4a-4aaa-b195-c0e1ca1794ab" : {
+      "Menu_item_7" : [
+        "02e8f840-27b7-4797-b1f5-d637b2e90d79"
+      ],
+      "Menu_item_6" : [
+        "0447e155-5a47-452b-95d9-5d9daad2ce6e"
+      ],
+      "Menu_item_5" : [
+        "e4d54d0e-c142-4798-9b6d-5f8e454cd47b"
+      ],
+      "Label_9" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ]
+    },
+    "5e679026-c831-4fc9-a5dd-9e8c97172b74" : {
+      "Button_1" : [
+        "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8"
+      ],
+      "Button_2" : [
+        "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8"
+      ]
+    },
+    "56cc956b-de07-41c2-96f5-64ec7af5467c" : {
+      "Button_1" : [
+        "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8"
+      ],
+      "Button_2" : [
+        "6d2d7ab8-04ab-49f1-83b9-cfd2468fabe8"
+      ]
+    },
+    "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa" : {
+      "Label_7" : [
+        "fbb22a47-2718-49a3-a1e6-f712249f5120"
+      ],
+      "Label_14" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_15" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Button_1" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ]
+    },
+    "5d024353-5558-47f0-81d7-4ee7aa581184" : {
+      "Button_2" : [
+        "fd20adf0-5afd-4d50-af13-07534c210383"
+      ],
+      "Image_1" : [
+        "c6af2bd8-0711-4b95-89f1-8a9c0036ded7"
+      ],
+      "Image_2" : [
+        "d69a4cbf-1844-45c3-9db6-887cad5a0d1b"
+      ]
+    },
+    "fbb22a47-2718-49a3-a1e6-f712249f5120" : {
+      "Label_24" : [
+        "9d1b7a89-43da-48f3-9a62-02728d968463"
+      ],
+      "Label_25" : [
+        "d1980101-0a74-45e0-8bfb-dcd9d9f5b8a6"
+      ],
+      "Label_26" : [
+        "4e0010c3-9d28-46a7-a703-cbdf8ef39aaa"
+      ],
+      "Button_1" : [
+        "1c4f49c6-a53c-4d15-bb26-529d86cf97d5"
+      ]
+    },
+    "162570a7-0558-40f7-824c-e393b1433cb6" : {
+      "Button_1" : [
+        "5d024353-5558-47f0-81d7-4ee7aa581184"
+      ],
+      "Button_2" : [
+        "5d024353-5558-47f0-81d7-4ee7aa581184"
+      ]
+    },
+    "e7ba14db-e912-4fe9-b55f-f0b9e60a7ae4" : {
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
---
+++ ProjetIHMPrototype/Prototype2/review/includes/include-1515720080973.js
@@ -0,0 +1,4 @@
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/_jim/javascript/jim-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scenarios/function-jim-links1515720080973.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/prototype-1515720080973.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scale-1515720080973.js'></script>");
